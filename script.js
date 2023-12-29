db3eb3e1a8962ab1443077b6e13a2f7f55cc62a3
Joko Santoso <joko74479@gmail.com>
2023-12-29 19:23:52 +0700

    update the code to clean

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const key = document.getElementById('key'),
 	password = document.getElementById('password');
 key.addEventListener('click', function () {
 	password.focus();
-	if (password.type == 'password') {
-		password.type = 'text';
-		key.classList.replace('bx-lock-alt', 'bx-lock-open-alt');
-	} else {
-		password.type = 'password';
-		key.classList.replace('bx-lock-open-alt', 'bx-lock-alt');
-	}
+	// password type
+	password.type = password.type === 'password' ? 'text' : 'password';
+	// lock element
+	key.className = `bx bx-lock-${password.type === 'password' ? 'alt' : 'open-alt'}`;
 });
